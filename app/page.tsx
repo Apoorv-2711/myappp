f83a5e7e3d6920fbf6026fb9709304df65463d00
apoorv-2711 <apoorvjain162@gmail.com>
2025-08-04 15:44:41 +0530

    Remove unnecessary whitespace in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import DataList from "@/components/data-list";
 export default function Home() {
   return (
     <div className=" min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     
       <DataList />
     </div>
   );
